0c0a2371db9dd6df0d37301e8b8600c91ee3559c
bonyvah <bekbolsunysmanov07@gmail.com>
2026-03-22 14:26:19 +0600

    add create user flow

--- backend/crud/users.py
+++ backend/crud/users.py
@@ -1,14 +1,15 @@
 from sqlmodel import Session, select
 from models.users import User
+from utils.security import hash_password
 
 
 def get_user_by_email(session: Session, email: str):
     return session.exec(select(User).where(User.email == email)).first()
 
 
-# def create_user(session: Session, email: str, password: str):
-
-#     session.add(user)
-#     session.commit()
-#     session.refresh(user)
-#     return user
+def create_user(session: Session, email: str, password: str):
+    user = User(email=email, hashed_password=hash_password(password))
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from schemas.users import UserCreate, UserRead
-from crud.users import get_user_by_email
+from crud.users import get_user_by_email, create_user
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 
 
 @router.post("/register/", response_model=UserRead)
@@ -10,4 +10,6 @@ def register(user_in: UserCreate):
     user = get_user_by_email(user_in.email)
     if user:
         raise HTTPException(status_code=400, detail="email is already registered")
+    user = create_user(email=user_in.email, password=user_in.password)
+
     return user
