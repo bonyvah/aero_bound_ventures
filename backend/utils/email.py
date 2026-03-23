5aab9d58bb6fde90b4208d0a9d0c57f7e345f7d1
bonyvah <bekbolsunysmanov07@gmail.com>
2026-03-23 11:32:59 +0600

    setup email conf

--- backend/utils/email.py
+++ backend/utils/email.py
@@ -1,17 +1,28 @@
-from email.message import EmailMessage
+from fastapi_mail import ConnectionConfig
 from aiosmtplib import SMTP
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT", "587")),
+    MAIL_SERVER=os.getenv("MAIL_HOST"),
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+)
+
 
 async def send_email_async(subject, recipient, body):
-    message = EmailMessage()
-    message["From"] = f"{os.getenv('MAIL_FROM')}"
-    message["To"] = recipient
-    message["Subject"] = subject
-    message.set_content(body)
+    # message["From"] = f"{os.getenv('MAIL_FROM')}"
+    # message["To"] = recipient
+    # message["Subject"] = subject
+    # message.set_content(body)
 
     smtp = SMTP(
         hostname=os.getenv("MAIL_HOST"),
