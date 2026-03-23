66c9674bfa7f97d3daef627d6eb821734c5f1016
bonyvah <bekbolsunysmanov07@gmail.com>
2026-03-23 11:22:56 +0600

    add background task

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,25 +1,30 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, BackgroundTasks, Depends
+from sqlalchemy.orm import Session
 from backend.schemas.users import UserCreate, UserRead
 from backend.crud.users import get_user_by_email, create_user
+from backend.crud.database import get_session
 from backend.utils.email import send_email_async
 
 router = APIRouter(prefix="/api")
 
 
 @router.post("/register/", response_model=UserRead)
-async def register(user_in: UserCreate):
-    user = get_user_by_email(user_in.email)
+async def register(
+    background_tasks: BackgroundTasks,
+    user_in: UserCreate,
+    session: Session = Depends(get_session),
+):
+    user = get_user_by_email(session, user_in.email)
     if user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="email is already registered",
         )
-    user = create_user(email=user_in.email, password=user_in.password)
+    user = create_user(session, user_in.email, user_in.password)
 
-    await send_email_async(
-        subject="Welcome to the Aero Bound Ventures!",
-        recipient=user_in.email,
-        body=f"Hello {user_in.email}, \n\nThank you for registering.",
-    )
+    subject = ("Welcome to the Aero Bound Ventures!",)
+    recipients = ([user_in.email],)
+    body_text = (f"Hello {user_in.email}, \n\nThank you for registering.",)
+    background_tasks.add_task(send_email_async, subject, recipients, body_text)
 
     return user
